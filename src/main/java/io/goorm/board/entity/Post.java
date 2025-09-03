907958f08304e7ecafd48d00a68a19197ad70b2b
topolo.edu <topolo.edu@gmail.com>
2025-09-03 12:38:12 +0900

    오류 프로퍼티 추가

--- src/main/java/io/goorm/board/entity/Post.java
+++ src/main/java/io/goorm/board/entity/Post.java
@@ -20,18 +20,18 @@ public class Post {
     @GeneratedValue(strategy = GenerationType.IDENTITY)  // 자동 증가
     private Long seq;  // 게시글 번호
 
-    @NotBlank(message = "제목은 필수입니다")
-    @Size(min = 1, max = 200, message = "제목은 1~200자로 입력해주세요")
+    @NotBlank(message = "{post.title.required}")
+    @Size(min = 1, max = 200, message = "{post.title.size}")
     @Column(nullable = false, length = 200)
     private String title;  // 제목
 
-    @NotBlank(message = "내용은 필수입니다")
-    @Size(min = 10, max = 4000, message = "내용은 10~4000자로 입력해주세요")
+    @NotBlank(message = "{post.content.required}")
+    @Size(min = 10, max = 4000, message = "{post.content.size}")
     @Column(nullable = false, columnDefinition = "TEXT")
     private String content;  // 내용
 
-    @NotBlank(message = "작성자는 필수입니다")
-    @Size(min = 2, max = 50, message = "작성자명은 2~50자로 입력해주세요")
+    @NotBlank(message = "{post.author.required}")
+    @Size(min = 2, max = 50, message = "{post.author.size}")
     @Column(nullable = false, length = 50)
     private String author;  // 작성자
 
